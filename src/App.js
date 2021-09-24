77b69133dfcfc451d5de26108d1f33ad4ee636f7
Evgenii <evtatdevelop@gmail.com>
2021-09-24 20:38:33 +0700

    refactoring App.js

--- src/App.js
+++ src/App.js
@@ -11,85 +11,42 @@ import userDataInputs from './components/InputDataUser/inputsUserDataList';
 import Spinner from './components/spinner';
 import Window from './components/window';
 import Error from './components/Error';
+
 export default class App extends Component {
 
   nameUser = React.createRef();
-
   service = new Service();
+  postDataInit = {   
+    email: '', ad_user: '', company_name: '', branch_name: '', div_name: '',
+    position_name: '', location: '', phone: '', sap_branch_name: '',
+  }
+  stateInit = {
+    userData: {},
+    postData: this.postDataInit,
+    companyList: {},    
+    branchList: {},    
+    DivisionList: {},    
+    loading: false,
+    error: false,
+    showWindow: false,
+  }
 
   state = {
     systemData: {},
-    userData: {},
-
-    loading: false,
-    error: false,
-    msgTime: 0,
-    msgData: {},
-    showWindow: false,
-   
-    email: '',
-    ad_user: '',
-    company_name: '',
-    branch_name: '',
-    div_name: '',
-    position_name: '',
-    location: '',
-    phone: '',
-    sap_branch_name: '',
-
-    companyList: {},
-  }
-
-  clear = () => {
-    this.setState({
-      userData: {},
-      loading: false,
-
-      email: '',
-      ad_user: '',
-      company_name: '',
-      branch_name: '',
-      div_name: '',
-      position_name: '',
-      location: '',
-      phone: '',
-      sap_branch_name: '',
-    });
+    msgTime: 0, msgData: {},
+    ...this.stateInit,
   }
 
   componentDidCatch() {
-    console.log('Samething goes wrong!');
     this.setState({error: true})
   }
   
-
   componentDidMount() {
     this.getSystemData(
       `${window.location.protocol}//${window.location.hostname}/`,
       `${window.location.pathname}`
     );
     this.getCompanies();
-  }
-
-  getUserData = (id) => {
-    this.loading();
-    this.service.getDataUser(id)
-      .then(userData => {
-        this.setState({
-          userData,
-          email: userData.email,
-          ad_user: userData.ad_user,
-          company_name: userData.company.name,
-          branch_name: userData.branch.name,
-          div_name: userData.div_name,
-          position_name: userData.position_name,
-          location: userData.location,
-          phone: userData.phone1,
-          sap_branch_name: userData.sap_branch.name,
-        });
-        this.noLoading();
-      })
-      .catch(this.onError);
   }
 
   getSystemData = (url, path) => {
@@ -102,6 +59,27 @@ export default class App extends Component {
       .catch(this.onError)
   }
 
+  getUserData = (id) => {
+    this.loading();
+    this.service.getDataUser(id)
+      .then(userData => {
+        const {email, ad_user, company: {name: company_name},
+                branch: {name: branch_name}, div_name, position_name,
+                location, phone1: phone, sap_branch: {name: sap_branch_name},
+              } = userData;
+
+        this.setState({
+          userData,
+          postData: { email, ad_user, company_name, branch_name, div_name,
+                      position_name, location, phone, sap_branch_name,
+                    }
+        });
+
+        this.noLoading();
+      })
+      .catch(this.onError);
+  }
+
   getCompanies = () => {
     this.service.getCompanies()
     .then(companyList => this.setState({companyList}))
@@ -110,15 +88,8 @@ export default class App extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    const{systemData, userData, email, ad_user, company_name, branch_name,
-       div_name, position_name, location, phone, sap_branch_name} = this.state;
-    const postData = {
-      ...systemData, ...userData, email, ad_user, company_name, branch_name, div_name, 
-      position_name, location, phone, sap_branch_name
-    };
-
     this.loading();
-    this.service.postForm(postData)
+    this.service.postForm(this.state.postData)
       .then(submitRequest => {
         this.showMessage(5000, submitRequest);
         this.clear();
@@ -128,22 +99,29 @@ export default class App extends Component {
       .catch(this.onError)
   } 
 
+  handlerInput = (e, prop) => {
+    const postData = this.state.postData;
+    postData[prop] = e.target.value;
+    this.setState({postData})
+  };
+
+  handlerClr = prop => {
+    const postData = this.state.postData;
+    postData[prop] = '';
+    this.setState({postData})
+  };
+
+  clear = () => this.setState({...this.state, ...this.stateInit,});
+  showMessage = (msgTime, msgData) => this.setState({msgTime, msgData});
+  loading = () => this.setState({loading: true})
+  noLoading = () => this.setState({loading: false})
+  showWindow = () => this.setState({showWindow: true});
+  hideWindow = () => this.setState({showWindow: false});
   onError = () => {
     this.setState({error: true});
     this.noLoading();
   }
-
-  showMessage = (msgTime, msgData) => this.setState({msgTime, msgData});
-
-  loading = () => {this.setState({loading: true})}
-  noLoading = () => {this.setState({loading: false})}
-
-  handlerInput = (e, prop) => this.setState({[prop]: e.target.value});
-  handlerClr = prop => this.setState({[prop]: ''});
-  handlerClick = (prop) => this.setState({showWindow: true});
   
-  handlerCloseWin = () => this.setState({showWindow: false});
- 
   render() {
     const {msgTime, msgData, loading, showWindow, error} = this.state;
     
@@ -152,12 +130,9 @@ export default class App extends Component {
     return (
       <div className="App">
         <h1>Test page</h1>
-        <form 
-          className="mainForm"
-          onSubmit={this.onSubmit}
-        >
-          <FormSet label="Employee info">
-            
+        <form className="mainForm" onSubmit={this.onSubmit}>
+
+          <FormSet label="Employee info">            
             <RowBox>
               <label className="rowLabel" htmlFor='userName'>Employee name</label>
               <NameSearch
@@ -173,28 +148,24 @@ export default class App extends Component {
             {userDataInputs.map((input, index) => 
               <InputDataUser
                 key = {index}
-                value = {this.state[input.option]}
+                value = {this.state.postData[input.option]}
                 option = {input.option}
                 label = {input.label}
                 readonly = {input.readonly}
                 placeholder = {input.placeholder}
-                arialabel = {input.arialabel}
                 handlerInput = {this.handlerInput}
                 handlerClr = {this.handlerClr}
-                handlerClick = {input.handlerClick ? this.handlerClick : ()=>{return}}
+                handlerClick = {input.handlerClick ? this.showWindow : ()=>{return}}
               />
             )}
 
           </FormSet>
 
           <Button label = "Apply" type = "submit"/>
-
           <Message data = {msgData} time = {msgTime}/>
-
-          {showWindow ? <Window handlerCloseWin={this.handlerCloseWin}/> : null}
-
           {loading ? <Spinner className="spinner"/> : null}
-
+          {showWindow ? <Window handlerCloseWin={this.hideWindow}/> : null}
+          
         </form>
       </div>
     );
--- src/components/InputDataUser/inputDataUser.js
+++ src/components/InputDataUser/inputDataUser.js
@@ -1,8 +1,6 @@
 import RowBox from '../rowBox';
 import Input from '../input';
 
-
-
 const InputDataUser = (props) => {
   const {value, option, label, readonly, placeholder, handlerInput, handlerClr, handlerClick} = props;
   
