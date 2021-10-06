1a8240ce2ab897c0dfbbd25afe18cfa14802bee6
Evgenii <evtatdevelop@gmail.com>
2021-10-06 11:41:34 +0700

    Mobail layout for main page

--- src/pages/main/mainPage.js
+++ src/pages/main/mainPage.js
@@ -1,24 +1,146 @@
 import classes from './main.module.scss'
 import {Link} from 'react-router-dom';
 
+
 const MainPage = props => {
   return (
     <main className={classes.main}>
-      <ul>
-        <li><Link to = '/resource' className={classes.link}>Requesting file and server resources</Link></li>
-        <li><Link to = '/workplace' className={classes.link}>Workplace organization request</Link></li>
-        <li><Link to = '/corpsystems/sap' className={classes.link}>Sap</Link></li>
-        <li><Link to = '/corpsystems/iais' className={classes.link}>iais</Link></li>
-        <li><Link to = '/corpsystems/teplosbyt' className={classes.link}>teplosbyt</Link></li>
-        <li><Link to = '/corpsystems/astep' className={classes.link}>astep</Link></li>
-        <li><Link to = '/corpsystems/citrix' className={classes.link}>citrix</Link></li>
-        <li><Link to = '/corpsystems/sed' className={classes.link}>sed</Link></li>
-        <li><Link to = '/corpsystems/seddeputy' className={classes.link}>seddeputy</Link></li>
-        <li><Link to = '/corpsystems/saperion' className={classes.link}>saperion</Link></li>
-        <li><Link to = '/corpsystems/diadocmail' className={classes.link}>diadocmail</Link></li>
-        <li><Link to = '/corpsystems/cert_ep' className={classes.link}>cert_ep</Link></li>
-        <li><Link to = '/corpsystems/sozvezdie' className={classes.link}>sozvezdie</Link></li>
-      </ul>      
+      
+      <div className={classes.groupBox}>
+        <input type="radio" id="workplace" name="groupSystems" value="workplace" className={classes.visuallyHidden}/>
+        <label htmlFor="workplace">Workplace organization and network resources</label>
+        <ul className={classes.systemList}>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/resource' className={classes.link}>Requesting file and server resources</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/workplace' className={classes.link}>Workplace organization request</Link>
+              <div className={classes.visualLink}></div>
+            </div>  
+          </li>
+        </ul>        
+      </div>
+
+
+      <div className={classes.groupBox}>
+        <input type="radio" id="corpsystems" name="groupSystems" value="corpsystems" className={classes.visuallyHidden}/>
+        <label htmlFor="corpsystems">Corporate information systems</label>
+        <ul className={classes.systemList}>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/sap' className={classes.link}>Sap</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/iais' className={classes.link}>iais</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/teplosbyt' className={classes.link}>teplosbyt</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/astep' className={classes.link}>astep</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/citrix' className={classes.link}>citrix</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/sed' className={classes.link}>sed</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/seddeputy' className={classes.link}>seddeputy</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/saperion' className={classes.link}>saperion</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/diadocmail' className={classes.link}>diadocmail</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/cert_ep' className={classes.link}>cert_ep</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/sozvezdie' className={classes.link}>sozvezdie</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+        </ul>         
+      </div>
+
+
+
+      <div className={classes.groupBox}>
+        <input type="radio" id="applications" name="groupSystems" value="applications" className={classes.visuallyHidden}/>
+        <label htmlFor="applications">Applied and information systems</label>
+        <ul className={classes.systemList}>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/seddeputy' className={classes.link}>seddeputy</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/saperion' className={classes.link}>saperion</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/diadocmail' className={classes.link}>diadocmail</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/cert_ep' className={classes.link}>cert_ep</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+          <li>
+            <div className={classes.linlBox}>
+              <Link to = '/corpsystems/sozvezdie' className={classes.link}>sozvezdie</Link>
+              <div className={classes.visualLink}></div>
+            </div>
+          </li>
+        </ul>      
+      </div>
+         
+
+     
     </main>
 
   )
