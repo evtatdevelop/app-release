e566ea79953a7c71dd9dcef23ece596d480e6c08
Evgenii <evtatdevelop@gmail.com>
2021-12-27 23:31:48 +0700

    Add styleas for agreement info

--- src/components/Agreements/agreements.js
+++ src/components/Agreements/agreements.js
@@ -15,19 +15,17 @@ export default class Agreements extends Component {
 
 
 
-  renderRoleAgree = roleAgreements => {
+  renderRoleAgree = (roleAgreements, roleId) => {
     console.log(roleAgreements);
     return(
       <div className={classes.oneRoleAgree}>
         {roleAgreements.map((agree, index) => {
-          const agreePersonDefault = agree.person[0]['name'];
           return(
             <div key={index} className={classes.rowTable}>
-              {/* {`${agree.asz10_order_seq} | ${agree.asz10_name} | ${agree.asz06_code_value} | ${agree.asz10_name} | `} */}
-              <p>{agree.asz10_order_seq}</p>
-              <p>{agree.asz10_name}</p>
-              <p>{agree.asz06_code_value}</p>
-              <p>{agreePersonDefault}</p>
+              <div>{agree.asz10_order_seq}</div>
+              <div>{agree.asz10_name}</div>
+              <div>{agree.asz06_code_value}</div>
+              <div>{this.renderPersonList(agree.person, agree.asz10_order_seq)}</div>
             </div>
           )
         })}
@@ -35,21 +33,41 @@ export default class Agreements extends Component {
     )
   }
 
+  renderPersonList = (personList, rowName) => {
+    return ( 
+      <ul>
+        {personList.map( item => {
+          return (
+          <li key = {item.id}>
+            {/* {item.name} */}
+            <input 
+              type='radio' 
+              id={item.id} 
+              name={rowName}
+              // onClick={() => this.handlerWindowClickLevel(row)}
+            />
+            <label htmlFor={item.id}>{item.name}</label>
+          </li>
+          )
+        })}
+      </ul>
+    )
+  }
 
   render() {
     const {system} = this.props;
     return(    
       <div className={classes.agreements}>
-        <h2>{system.full_name}</h2>
+        <h2 className={classes.systemFullName}>{system.full_name}</h2>
         <ul>
           {
             this.state.roles.map(role => {
               if (!role.role.id) return null;
               return(
                 <li key={role.id}>
-                  <h3>{`${role.group.name} / ${role.role.name}`}</h3>
+                  <h3 className={classes.roleFullName}>{`${role.group.name} / ${role.role.name}`}</h3>
                   {console.log(role.agreements.length)}
-                  {this.renderRoleAgree(role.agreements)}
+                  {this.renderRoleAgree(role.agreements, role.id)}
                 </li>
               )
             })
