53279df2b9108f94de489e06d7efbd260533e171
Evgenii <evtatdevelop@gmail.com>
2022-02-07 17:06:40 +0700

    output form filling result

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -34,9 +34,33 @@ class Message extends Component {
         ? <section className={classes.message}>
             {this.props.data
               ? Object.entries(this.props.data).map((row, index) => {
+                if ( typeof(row[1]) !== 'object')
                 return <p key={index} >{row[0]}: {row[1]}</p>
+                else {
+                  // console.log(row[1]);
+                  return (
+                    <>
+                      <p>Roles:</p>
+                      <ul>
+                        {row[1].map((item, index) => 
+                          <li key={index}>
+                            {`${item.group.name} / ${item.role.name}`}
+                            <p>Levels:</p>
+                            <ul>
+                              {item.levels.map((item, index1) => 
+                                <li key={index1}>{`${item.name}: ${item.value.join(', ')}`}</li>
+                              )}
+                            </ul>
+                          </li>
+                        )}
+                      </ul>                    
+                    </> 
+                  )
+                }
+                
               })
               : null} 
+            {/* {console.log(this.props.data)} */}
           </section>
         : null  
     )
